ec23d93468168ab8c51f9dda53d74bd1d5365d4d
NotAzebu <30387838+NotAzebu@users.noreply.github.com>
2017-08-05 00:34:38 +0900

    Add files via upload

---
+++ playerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start ()
+
+    public float jumpForce = 6.0f;
+    private Rigidbody2D rigidBody;
+	
+	
+    void awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+        {
+        if (Input.GetMouseButtonDown(0))
+            {
+            Debug.Log("spacebar pressed");
+            Jump();
+            }
+    }
+        void Jump()
+            {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+}
