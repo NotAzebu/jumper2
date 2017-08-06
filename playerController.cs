4e4685f22b4278e3e208aa24d570d85c4f9d73cc
NotAzebu <30387838+NotAzebu@users.noreply.github.com>
2017-08-06 18:16:28 +0900

    Add files via upload

--- playerController.cs
+++ playerController.cs
@@ -2,30 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
-
-	// Use this for initialization
-	//void Start ()
+public class playerController : MonoBehaviour
+{
+    
+    // Use this for initialization
+    //void Start ()
 
     public float jumpForce = 6.0f;
     private Rigidbody2D rigidBody;
-	
-	
-    void awake()
+    public float jumpRaycast = 0.6f;
+    public float runningSpeed = 1.0f;
+
+    //public bool isGrounded;
+    void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
+    {
+        if (rigidBody.velocity.x < runningSpeed)
         {
-        if (Input.GetMouseButtonDown(0))
-            {
-            Debug.Log("spacebar pressed");
-            Jump();
-            }
+            rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+        }
     }
-        void Jump()
-            {
+
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            Debug.Log("Jump button pressed");
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        if (IsGrounded())
+        {
             rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            }
+        }
+    }
+
+    public LayerMask groundLayer;
+
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, jumpRaycast, groundLayer.value))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
